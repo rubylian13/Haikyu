35674e228147e54de11e7dac6504d9ae6c274a95
rubylian <rubylian5813@gmail.com>
2024-03-30 02:19:28 +0800

    Initial Commit

---
+++ Haikyu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Haikyu
+//
+//  Created by Ruby Lien on 2024/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
